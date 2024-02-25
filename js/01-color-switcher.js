0d71a093bc9b6b632a1b635981e96e151fcbf45e
DamianNor <damian.szylar@yahoo.com>
2024-02-25 12:03:03 +0100

    Pierwszy commit projektu z Parcel

---
+++ js/01-color-switcher.js
@@ -0,0 +1,21 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const startButton = document.querySelector("[data-start]");
+const stopButton = document.querySelector("[data-stop]");
+
+let intervalId = null;
+
+startButton.addEventListener("click", () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+
+  startButton.disabled = true;
+});
+
+stopButton.addEventListener("click", () => {
+  clearInterval(intervalId);
+  startButton.disabled = false;
+});
